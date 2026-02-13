7addfd717ca25ccb8aee339e8815227c60a13365
matipepek <matias.pepek@gmail.com>
2026-02-13 14:51:38 -0300

    Agregadas sonidos.c y sonidos.h
    Bibliotecas destinadas al manejo de sonidos

---
+++ Proyecto/sonidos.c
@@ -0,0 +1,132 @@
+#include "sonidos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIXER_CANAL_LIBRE -1 // Reproduce en el primer canal libre disponible
+#define BUFFER_AUDIO 1024    // Tamanio del buffer de memoria
+#define ESTEREO 2            // 2 canales, sonido estereo
+
+struct sSonido{
+    Mix_Chunk* chunk;
+    //int esTono;   //Por ahora no vamos a generar ningún tono generado por buffer de audio, pero lo dejo en caso de que cambiemos de opinion
+};
+
+
+tFormatosSnd sonidos_inicializar(void)
+{
+    tFormatosSnd formatosSnd = SONIDO_WAV; // Soporte para WAV ya incluido
+    int flags = MIX_INIT_MP3 | MIX_INIT_OGG; //(Originalmente, en el ejemplo se usa int32_t, ver que es mejor)
+    int init = Mix_Init(flags); // Inicializa SDL_mixer, devuelve los flags inicializados exitosamente o 0 si falla
+
+    //Mascara de bits con la inicializacion y los formatos cargados, si la inicializacion fallo devuelve error
+    if ((init & flags) != flags)
+    {
+        fprintf(stderr,"Error: %s\n", Mix_GetError());
+    }
+
+    //Si la inicializacion coincide con MP3, lo asigna al formato de sonido. Lo mismo pasa con OGG, se suma a los formatos de sonido.
+    if (init & MIX_INIT_MP3)
+    {
+        formatosSnd |= SONIDO_MP3; //Hacer formatosSnd |= SONIDO_MP3 es lo mismo que hacer formatosSnd = formatosSnd |(OR) SONIDO_MP3
+    }
+
+    if (init & MIX_INIT_OGG)
+    {
+        formatosSnd |= SONIDO_OGG;
+    }
+
+    //Abre el dispositivo de audio en formato estereo
+    if(Mix_OpenAudio(FREC_MUESTREO, MIX_DEFAULT_FORMAT, ESTEREO, BUFFER_AUDIO)<0)
+    {
+        fprintf(stderr, "Error: No se pudo abrir el dispositivo de audio: %s\n", Mix_GetError());
+        Mix_Quit();
+        return SONIDO_ERR;
+    }
+
+    return formatosSnd;
+}
+
+tSonido* sonidos_cargar(const char *path)
+{
+    tSonido* sonido = malloc(sizeof(tSonido));
+    if(!sonido)
+        return NULL;
+
+    sonido->chunk = Mix_LoadWAV(path);
+    if (!sonido->chunk)
+    {
+        fprintf(stderr, "Fallo la carga del sonido \"%s:\" %s\n", path, Mix_GetError());
+        free(sonido);
+        return NULL;
+    }
+
+    return sonido;
+}
+
+//Revisar, no funciona
+/*void sonidos_reproducir(const tSonido *sonido, int32_t cantVeces) //(const tSonido *sonido, int32_t cantVeces)
+{
+    if (!sonido->chunk)
+    {
+        return;
+    }
+
+    int32_t loops; //el tipo de dato era originalmente int32_t
+
+    if (cantVeces == -1)
+    {
+        loops = -1; // -1 para reproducir de forma infinita
+    }
+    else
+    {
+        loops = (cantVeces > 0) ? (cantVeces - 1) : 0; // En mixer 0 para una vez, 1 para dos, etc.. y no acepta valores menores a -1
+    }
+
+    if (Mix_PlayChannel(MIXER_CANAL_LIBRE, sonido->chunk, loops) == -1)
+    {
+        fprintf(stderr, "No se pudo reproducir el sonido: %s\n", Mix_GetError());
+    }
+}*/
+
+void sonidos_destruir(tSonido *sonido)
+{
+    if (!sonido)
+    {
+        return;
+    }
+
+    /*
+        Comentado porque no utilizamos tonos, en caso de querer usarse la logica es la siguiente:
+
+        if (sonido->esTono)
+        {
+            if (sonido->chunk)
+            {
+                if (sonido->chunk->abuf)
+                {
+                    free(sonido->chunk->abuf);
+                }
+                free(sonido->chunk);
+            }
+        }
+        else
+        {
+            if (sonido->chunk)
+            {
+                Mix_FreeChunk(sonido->chunk);
+            }
+        }
+    */
+    if (sonido->chunk)
+    {
+        Mix_FreeChunk(sonido->chunk);
+    }
+    free(sonido);
+}
+
+void sonidos_finalizar(void)
+{
+    Mix_HaltChannel(-1); //Se usa -1 para detener todos los canales
+    Mix_CloseAudio();
+    Mix_Quit();
+}
---
+++ Proyecto/sonidos.h
@@ -0,0 +1,41 @@
+#ifndef SONIDOS_H_INCLUDED
+#define SONIDOS_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+#define FREC_MUESTREO 44100
+
+//Formatos de audio soportados
+typedef enum {
+    SONIDO_ERR = -1,
+    SONIDO_WAV =  0,
+    SONIDO_MP3 =  1,
+    SONIDO_OGG =  2,
+} tFormatosSnd;
+
+//Se usa Mix_Chunk porque son efectos cortos cargados desde la unidad de almacenamiento
+typedef struct sSonido tSonido;
+
+tFormatosSnd sonidos_inicializar(void);
+
+/*
+    IMPORTANTE!!!!: los sonidos deberian estar dentro de una estructura tJuego definida en la logica de "juego.h"
+    donde tJuego es la estructura principal del juego que contiene el estado del mismo y sus recursos (para este caso,
+    deberia tener los campos tSonido* seleccion, tSonido* acierto y tSonido* fallo
+
+    (VER JUEGO.H EN SDL_EJEMPLO_AVANZADO)
+*/
+//Carga un archivo de audio desde el almacenamiento
+tSonido* sonidos_cargar(const char *path);
+
+//Reproduce un sonido "n" veces, determinado por el parametro cantVeces
+//void sonidos_reproducir(const tSonido *sonido, int32_t cantVeces) //(const tSonido *sonido, int32_t cantVeces)
+
+//Libera memoria asociada al sonido
+void sonidos_destruir(tSonido *sonido);
+
+//Finaliza correctamente SDL_mixer, cerrando primero los canales de audio, luego cerrando el mixer y finalmente desinicializandolo
+void sonidos_finalizar(void);
+
+#endif // SONIDOS_H_INCLUDED
