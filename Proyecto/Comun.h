01b7e554b5f7edce023e3426afeab9248fd0ff4e
IvannCen <ivancenturion0840@gmail.com>
2026-02-13 00:45:35 -0300

    Update Comun.h

--- Proyecto/Comun.h
+++ Proyecto/Comun.h
@@ -7,20 +7,21 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+
 #define ANCHOVENTANA 1366
 #define ALTOVENTANA 768
 
 #define ANCHOCARTA 130
 #define ALTOCARTA 170
 #define SEPARACIONCARTA 15
-#define MARGENX 180
-#define MARGENY 50
-#define HUD_ALTO 150
 
 #define ALTOINTERFAZ 80
 #define INTERFAZMARGENLATERAL 30 // Margen izquierdo/derecho para textos
+#define INTERFAZMARGENSUPERIOR 20
 
 #define CANTIDADIMAGENES 10
+
+#define CANTIDADNIVELES 2
 
 #define DELAY 1000 //1000 milisegundos es 1 segundo
 
